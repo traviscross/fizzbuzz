3ada481e0722ffbe0302db708b13aefabd59086e
Travis Cross <tc@traviscross.com>
2011-10-05 17:40:43 +0000

    Let user optionally specify start and stop values on command line

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -3,6 +3,10 @@
 
 int main (int argc, char **argv) {
   int i, start=1, stop=100;
+  if (argc > 1)
+    start=atoi(argv[1]);
+  if (argc > 2)
+    stop=atoi(argv[2]);
   for (i=start; i<=stop; i++) {
     if ((i % 3) == 0 && (i % 5) == 0)
       printf("FizzBuzz,");
