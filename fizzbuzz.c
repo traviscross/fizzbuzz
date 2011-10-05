948c3a1c50d5479bf171d59931665749da04dec4
Travis Cross <tc@traviscross.com>
2011-10-05 17:33:14 +0000

    Add first version of fizzbuzz

---
+++ fizzbuzz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv) {
+  int i;
+  for (i=1; i<=100; i++) {
+    if ((i % 3) == 0 && (i % 5) == 0)
+      printf("FizzBuzz,");
+    else if ((i % 3) == 0)
+      printf("Fizz,");
+    else if ((i % 5) == 0)
+      printf("Buzz,");
+    else
+      printf("%d,", i);
+  }
+  printf("\n");
+  return 0;
+}
+
