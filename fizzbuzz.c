008e426d6122f1aafb784ed9f892f1f2874335af
Travis Cross <tc@traviscross.com>
2011-10-06 01:13:48 +0000

    Whitespace

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -8,18 +8,10 @@ int main (int argc, char **argv) {
   if (argc > 2)
     stop=atoi(argv[2]);
   for (i=start; i<=stop; i++, x=0) {
-    if ((i % 3) == 0) {
-      printf("Fizz");
-      x=1;
-    }
-    if ((i % 5) == 0) {
-      printf("Buzz");
-      x=1;
-    }
-    if (!x)
-      printf("%d", i);
-    if (i != stop)
-      printf(",");
+    if ((i % 3) == 0) { x=1; printf("Fizz"); }
+    if ((i % 5) == 0) { x=1; printf("Buzz"); }
+    if (!x) printf("%d", i);
+    if (i != stop) printf(",");
   }
   printf("\n");
   return 0;
